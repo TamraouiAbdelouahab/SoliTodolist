e12418ce1ca757589d2125c06f2e23fbfdfe9c8e
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-04-08 14:03:57 +0100

    save

--- app/src/main/java/com/example/solitodolist/MainActivity.kt
+++ app/src/main/java/com/example/solitodolist/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import com.example.solitodolist.ui.theme.SoliTodoListTheme
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.solitodolist.ui.components.Header
 import com.example.solitodolist.ui.components.Footer
+import com.example.solitodolist.ui.components.TaskScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +27,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SoliTodoListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            //    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            //        Greeting(
+            //            name = "Android",
+            //            modifier = Modifier.padding(innerPadding)
+            //        )
+            //    }
+                AppSoliToDo()
             }
         }
     }
@@ -39,7 +46,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 @Composable
-fun AppSoliToDo()
+fun AppSoliToDo(
+    navController: NavHostController = rememberNavController(),
+)
 {
     Scaffold(
         topBar = {
@@ -48,9 +57,25 @@ fun AppSoliToDo()
         bottomBar = {
             Footer()
         }
+
     ){innerPadding->
-        Text(text = "Content" , modifier = Modifier.padding(innerPadding))
+        NavHost(
+            navController = navController,
+            startDestination = TodoScreens.Bakkali.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = TodoScreens.Tasks.name)
+            {
+                TaskScreen("this is Tasks Screen",{ navController.navigate(TodoScreens.Bakkali.name) })
+            }
+            composable(route = TodoScreens.Bakkali.name)
+            {
+                TaskScreen("this is Bakkali Screen",{ navController.navigate(TodoScreens.Tasks.name) })
+            }
+        }
     }
+
+
 }
 
 @Preview(showBackground = true)
@@ -59,4 +84,10 @@ fun GreetingPreview() {
     SoliTodoListTheme {
         AppSoliToDo()
     }
+}
+
+
+enum class TodoScreens{
+    Tasks,
+    Bakkali
 }
--- app/src/main/java/com/example/solitodolist/ui/components/header.kt
+++ app/src/main/java/com/example/solitodolist/ui/components/header.kt
@@ -1,10 +1,23 @@
 package com.example.solitodolist.ui.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Header(){
-    Text(text = "TopApp")
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text("< Lists")
+            }
+        )
 }
